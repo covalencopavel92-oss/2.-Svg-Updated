e49155647374512c8b0446b5d7851f080e1363e9
covalencopavel92-oss <covalencopavel92@gmail.com>
2026-04-12 21:59:33 +0300

    back to back

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,6 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   site: 'https://covalencopavel92-oss.github.io',
-  base: '/2.-Svg-Updated',
     
   prefetch: {
     // Removed prefetchAll: true to save bandwidth and speed up initial load
