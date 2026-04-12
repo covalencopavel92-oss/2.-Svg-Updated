1dd6610ec665bab964bd274c21a1a92fe8cbb021
covalencopavel92-oss <covalencopavel92@gmail.com>
2026-04-12 13:07:32 +0300

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,10 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
+
+  site: 'https://[your-username].github.io',
+  base: '/[repository-name]',
+    
   prefetch: {
     // Removed prefetchAll: true to save bandwidth and speed up initial load
     defaultStrategy: 'hover'
